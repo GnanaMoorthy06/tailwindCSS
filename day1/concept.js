e798db96aa22cf9ece26cc9e7f95f536bdb55872
GNANA MOORTHY D <129540732+GnanaMoorthy06@users.noreply.github.com>
2026-05-06 01:02:12 +0530

    CDN

---
+++ day1/concept.js
@@ -0,0 +1,25 @@
+/**
+ 
+
+ * Play CDN
+ -----------
+
+ NOTE : you must know HTML / CSS before learing the taileind CSS
+
+ S1 => go to https://tailwindcss.com/docs/installation/play-cdn website 
+
+ S2 => copy  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+ S3 => paste it inside the head tag
+
+ S4 => copy  <h1 class="text-3xl font-bold underline">
+             Hello world!
+             </h1>
+
+S5 => patse it inside BODY tag 
+
+S6 => Now u can see the difference
+
+NOTE : You can see difference in Before.png / After.png IMAGE
+
+ */
